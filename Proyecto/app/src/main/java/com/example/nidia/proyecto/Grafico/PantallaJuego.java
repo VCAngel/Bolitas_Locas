ecc117bd1d686cfff3a3bc61e7a33634f990e703
Nidia08 <gleznida_@outlook.com>
2020-03-19 13:09:54 -0700

    Actualizacion del proyeecto
    Se agrego fondo con gif, conexion a bases de datos y detalles en ids

--- Proyecto/app/src/main/java/com/example/nidia/proyecto/Grafico/PantallaJuego.java
+++ Proyecto/app/src/main/java/com/example/nidia/proyecto/Grafico/PantallaJuego.java
@@ -1,15 +1,55 @@
 package com.example.nidia.proyecto.Grafico;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nidia.proyecto.R;
 
 public class PantallaJuego extends AppCompatActivity {
+    TextView pjUserName, pjPuntuacion;
+    Button pjSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_juego);
+        pjUserName = findViewById(R.id.pjUser);
+        pjPuntuacion = findViewById(R.id.pjPuntuacion);
+        pjSalir = findViewById(R.id.pjSalir);
 
+        pjSalir.setOnClickListener(onClickListener);
+
+        String pjrUser = getIntent().getStringExtra("pjrUser");
+        String pjrPuntuacion = getIntent().getStringExtra("pjrPuntuacion");
+        pjUserName.setText(pjrUser);
+        pjPuntuacion.setText(pjrPuntuacion);
+        Toast.makeText(this, pjrPuntuacion, Toast.LENGTH_SHORT).show();
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            pressed(view);
+        }
+    };
+    private void pressed(View view){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Importante");
+        dialog.setMessage("¿Seguro que desea salir de este juego?");
+        dialog.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                System.exit(0);
+            }
+        });
+        dialog.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
--- Proyecto/app/src/main/java/com/example/nidia/proyecto/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/nidia/proyecto/MainActivity.java
@@ -4,28 +4,29 @@ import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.nidia.proyecto.Grafico.LoginUsuarios;
 import com.example.nidia.proyecto.Grafico.RegistroUsuarios;
 
 public class MainActivity extends Activity {
     EditText user, pass;
-    Button registro, log;
-
+    Button mRegistrarse, mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        registro = findViewById(R.id.registrarse);
-        log = findViewById(R.id.login);
+        mRegistrarse = findViewById(R.id.mRegistrarse);
+        mLogin = findViewById(R.id.mLogin);
 
-        registro.setOnClickListener(onClickListener);
-        log.setOnClickListener(onClickListener);
+        mRegistrarse.setOnClickListener(onClickListener);
+        mLogin.setOnClickListener(onClickListener);
 
         /*user.findViewById(R.id.user);
         pass.findViewById(R.id.lpass);*/
@@ -40,13 +41,15 @@ public class MainActivity extends Activity {
 
     private void pressed(View view){
         switch(view.getId()){
-            case R.id.registrarse:
+            case R.id.mRegistrarse:
                 Intent intent1 = new Intent(this, RegistroUsuarios.class);
                 startActivity(intent1);
+                finish();
                 break;
-            case R.id.login:
+            case R.id.mLogin:
                 Intent intent2 = new Intent(this, LoginUsuarios.class);
                 startActivity(intent2);
+                finish();
                 break;
         }
     }
